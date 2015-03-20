4d97199c0a2be4f266374b704d2d8c2e7e34193d
tom2tom <tpgww@onepost.net>
2015-03-21 10:55:53 +1100

    update licence text

--- lib/gateways/class.googlevoice_sms_gateway.php
+++ lib/gateways/class.googlevoice_sms_gateway.php
@@ -1,39 +1,32 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module: CGSMS (c) 2010 by Robert Campbell 
-#         (calguy1000@cmsmadesimple.org)
-#  An addon module for CMS Made Simple to provide the ability for other
-#  modules to send SMS messages
-#
+# Module: CGSMS (C) 2010-2015 Robert Campbell (calguy1000@cmsmadesimple.org)
+# An addon module for CMS Made Simple to provide the ability for other
+# modules to send SMS messages
 #-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
-# This project's homepage is: http://www.cmsmadesimple.org
-#
+# CMS Made Simple (C) 2005-2015 Ted Kulp (wishy@cmsmadesimple.org)
+# Its homepage is: http://www.cmsmadesimple.org
 #-------------------------------------------------------------------------
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin 
-# section that the site was built with CMS Made simple.
+# This file is part of an addon module for CMS Made Simple.
+# As a special extension to the AGPL, you may not use this file in any
+# non-GPL version of CMS Made Simple, or in any version of CMS Made Simple
+# that does not indicate clearly and obviously in its admin section that
+# the site was built with CMS Made Simple.
 #
-# This program is distributed in the hope that it will be useful,
+# This file is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# Read the Licence online: http://www.gnu.org/licenses/licenses.html#AGPL
 #-------------------------------------------------------------------------
 #END_LICENSE
+
 class googlevoice_sms_gateway extends cgsms_sender_base
 {
   private $_rawstatus;
