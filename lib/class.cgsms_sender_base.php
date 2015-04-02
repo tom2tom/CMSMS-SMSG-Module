f7789e09a9100cc9502a47ba0f7abde9b17b087a
tom2tom <tpgww@onepost.net>
2015-04-02 15:28:56 +1100

    cleanups, initial API text

--- lib/class.cgsms_sender_base.php
+++ lib/class.cgsms_sender_base.php
@@ -43,10 +43,10 @@ abstract class cgsms_sender_base
   const DELIVERY_BILLING = 'sms_delivery_billing';
   const DELIVERY_OTHER   = 'sms_delivery_other';
 
-  private $_msg;
+  private $_module;
   private $_num;
   private $_fromnum;
-  private $_module;
+  private $_msg;
   private $_statusmsg;
   protected $_use_curl;
   protected $_status;
@@ -55,34 +55,34 @@ abstract class cgsms_sender_base
 
   function __construct(&$module)
   {
-    $this->_num = '';
-    $this->_msg = '';
     $this->_module = $module;
-    $this->_use_curl = '';
-    $this->_status = self::STAT_NOTSENT;
-    $this->_statusmsg = '';
+	self::reset();
   }
 
-
+  /**
+  reset:
+  Clear all cached data
+  */
   public function reset()
   {
     $this->_num = '';
+    $this->_fromnum = '';
     $this->_msg = '';
-    $this->_use_curl = '';
+    $this->_use_curl = 0;
     $this->_status = self::STAT_NOTSENT;
     $this->_statusmsg = '';
   }
 
-
-  public function use_curl($flag = true)
+  public function use_curl($flag = TRUE)
   {
-    if( $flag )
-      $this->_use_curl = 1;
-    else
-      $this->_use_curl = 0;
+    $this->_use_curl = ( $flag ) ? 1 : 0;
   }
 
-
+  /**
+  set_msg:
+  @msg: the body-content of the message to be sent, including only SMS-valid
+   characters, and of suitable length
+  */
   public function set_msg($msg)
   {
     $this->_msg = $msg;
@@ -94,7 +94,11 @@ abstract class cgsms_sender_base
     return $this->_msg;
   }
 
-
+  /**
+  set_num:
+  @num: destination phone-number (or separated numbers, if the gateway supports
+   batching) appropriately formatted for the gateway
+  */
   public function set_num($num)
   {
     $this->_num = $num;
@@ -106,6 +110,10 @@ abstract class cgsms_sender_base
     return $this->_num;
   }
 
+  /**
+  set_from:
+  @from: the source phone-number (if the gateway supports that) appropriately formatted
+  */
   public function set_from($from)
   {
     if($from)
@@ -124,19 +132,28 @@ abstract class cgsms_sender_base
     $this->_status = $stat;
   }
 
-
+  /**
+  get_status:
+  Returns string describing (short-form) status, one of the const's defined above
+  */
   public function get_status()
   {
     return $this->_status;
   }
 
-
+  /**
+  get_statusmsg:
+  Returns string ...
+  */
   public function get_statusmsg()
   {
     return $this->_statusmsg;
   }
 
-
+  /**
+  send:
+  Initiate the message transmission (after all relevant parameters are set up)
+  */
   public function send()
   {
     $this->_smsid = '';
@@ -178,51 +195,45 @@ abstract class cgsms_sender_base
 	return $success;
   }
 
-
+  //for internal use only
+  //get parameter stored (for some gateways) when operation result-message was parsed
   public function get_smsid()
   {
     return $this->_smsid;
   }
-
 
   protected function &get_module()
   {
     return $this->_module;
   }
 
-
+  //performs the send, may need to be over-ridden in some gateway-specific subclasses
   protected function _command($cmd)
   {
     $this->_check_curl();
     $res = '';
     if( $this->_use_curl == 0 )
-      {
-	$res = $this->_send_fopen($cmd);
-      }
+      $res = $this->_send_fopen($cmd);
     else
-      {
-	$res = $this->_send_curl($cmd);
-      }
+      $res = $this->_send_curl($cmd);
     debug_to_log('cgsms_sender_base - command = '.$cmd.' res = '.$res);
     return $res;
   }
 
-
   private function _send_fopen($cmd)
   {
-    $result = '';
+    $res = '';
     $fh = @fopen ($cmd, 'r');
     if ($fh) {
       while ($line = @fgets($fh,1024)) {
-	$result .= $line;
+       $res .= $line;
       }
       fclose ($fh);
-      return $result;
+      return $res;
     } else {
       return FALSE;
     }
   }
-
 
   private function _send_curl($cmd)
   {
@@ -230,44 +241,79 @@ abstract class cgsms_sender_base
     curl_setopt ($ch, CURLOPT_HEADER, 0);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER,1);
     curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER,0);
-//     if ($this->curl_use_proxy) {
-//       curl_setopt ($ch, CURLOPT_PROXY, $this->curl_proxy);
-//       curl_setopt ($ch, CURLOPT_PROXYUSERPWD, $this->curl_proxyuserpwd);
-//     }
-    $result=curl_exec ($ch);
+//  if ($this->curl_use_proxy) {
+//    curl_setopt ($ch, CURLOPT_PROXY, $this->curl_proxy);
+//    curl_setopt ($ch, CURLOPT_PROXYUSERPWD, $this->curl_proxyuserpwd);
+//  }
+    $res = curl_exec ($ch);
     curl_close ($ch);
-    return $result;
+    return $res;
   }
-
 
   private function _check_curl()
   {
-    if( $this->_use_curl == '' )
+    if( $this->_use_curl == FALSE )
       {
-	$this->_use_curl = 0;
-	if( extension_loaded('curl') )
-	  {
-	    $this->_use_curl = 1;
-	  }
+        if( extension_loaded('curl') )
+          $this->_use_curl = 1;
       }
   }
 
+  /**
+  process_delivery_report:
+  */
   public function process_delivery_report()
   {
     return $this->_process_delivery_report();
   }
 
+  /**
+  get_name:
+  Returns string which is the (un-translated) gateway identifier
+  */
   abstract public function get_name();
 
+  /**
+  get_description:
+  Returns string which is a (translated) brief description of the gateway
+  */
   abstract public function get_description();
 
+  /**
+  support_custom_sender:
+  Returns boolean TRUE/FALSE according to whether the gateway allows use of
+  a user-specified source-phone-number (which might need to be a number pre-arranged
+  with the gateway supplier)
+  */
   abstract public function support_custom_sender();
 
+  /**
+  require_country_prefix:
+  Returns boolean TRUE/FALSE according to whether a country-code prefix for
+  must be prepended to destination phone-numbers
+  */
   abstract public function require_country_prefix();
 
+  /**
+  require_plus_prefix:
+  Returns boolean TRUE/FALSE according to whether the country-code prefix for
+  destination numbers must begin with a '+' character
+  */
   abstract public function require_plus_prefix();
 
+  /**
+  multi_number_separator:
+  Returns string containing character to be used for separating numbers in
+  multi-destination 'to' values, or FALSE if only single-destination is allowed
+  */
   abstract public function multi_number_separator();
+
+  /**
+  support_mms:
+  Returns boolean TRUE/FALSE according to whether the gateway supports automatic
+  partitioning of messages longer than 160 chars into a series of shorter parts
+  */
+  abstract public function support_mms();
 
   abstract protected function setup();
 
@@ -277,10 +323,17 @@ abstract class cgsms_sender_base
 
   abstract protected function _process_delivery_report();
 
+  //for internal use only
+  //setup and display gateway-specific details for defaultadmin action
   abstract public function get_setup_form();
 
+  //for internal use only
+  //process gateway-specific details after 'submit' in defaultadmin action
   abstract public function handle_setup_form($params);
 
+  /**
+  get_raw_status:
+  */
   abstract public function get_raw_status();
 } // end of class
 #
