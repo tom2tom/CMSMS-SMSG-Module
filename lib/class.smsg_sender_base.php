642cbe837ada71973bd626ec605b96389900a7a2
tom2tom <tpgww@onepost.net>
2015-04-04 21:45:10 +1100

    rename file to conform to included class

--- lib/class.smsg_sender_base.php
+++ lib/class.smsg_sender_base.php
@@ -1,7 +1,7 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module: CGSMS (C) 2010-2015 Robert Campbell (calguy1000@cmsmadesimple.org)
+# Module: SMSG (C) 2010-2015 Robert Campbell (calguy1000@cmsmadesimple.org)
 # An addon module for CMS Made Simple to provide the ability for other
 # modules to send SMS messages
 #-------------------------------------------------------------------------
@@ -27,7 +27,7 @@
 #-------------------------------------------------------------------------
 #END_LICENSE
 
-abstract class cgsms_sender_base
+abstract class smsg_sender_base
 {
   const STAT_OK = 'sms_sent';
   const STAT_NOTSENT = 'sms_notsent';
@@ -56,7 +56,7 @@ abstract class cgsms_sender_base
   function __construct(&$module)
   {
     $this->_module = $module;
-	self::reset();
+    self::reset();
   }
 
   /**
@@ -162,37 +162,37 @@ abstract class cgsms_sender_base
     $this->setup();
     if( $this->_num == '' || $this->_msg == '' )
       {
-	$this->_status = self::STAT_ERROR_INVALID_DATA;
-	return FALSE;
+        $this->_status = self::STAT_ERROR_INVALID_DATA;
+        return FALSE;
       }
 
-    if( !cgsms_utils::ip_can_send(getenv('REMOTE_ADDR')) )
+    if( !smsg_utils::ip_can_send(getenv('REMOTE_ADDR')) )
       {
-	$this->_status = self::STAT_ERROR_LIMIT;
-	return FALSE;
+        $this->_status = self::STAT_ERROR_LIMIT;
+        return FALSE;
       }
 
     // next prepare the output.
     $cmd = $this->prep_command();
     if( $cmd === FALSE || $cmd == '' )
       {
-	$this->_status = self::STAT_ERROR_INVALID_DATA;
-	return FALSE;
+        $this->_status = self::STAT_ERROR_INVALID_DATA;
+        return FALSE;
       }
 
     // send it.
     $res = $this->_command($cmd);
 
-	// interpret result.
-	$this->parse_result($res);
-	$this->_statusmsg = cgsms_utils::get_msg($this,$this->_num,$this->_status,$this->_msg,$this->get_raw_status());
-	$success = ($this->_status == self::STAT_OK);
-	if( $success )
-	  {
-	    cgsms_utils::log_send(getenv('REMOTE_ADDR'),$this->_num,$this->_msg);
-		audit('',$this->get_module()->GetName(),$this->_statusmsg);
-	  }
-	return $success;
+    // interpret result.
+    $this->parse_result($res);
+    $this->_statusmsg = smsg_utils::get_msg($this,$this->_num,$this->_status,$this->_msg,$this->get_raw_status());
+    $success = ($this->_status == self::STAT_OK);
+    if( $success )
+      {
+        smsg_utils::log_send(getenv('REMOTE_ADDR'),$this->_num,$this->_msg);
+        audit('',$this->get_module()->GetName(),$this->_statusmsg);
+      }
+    return $success;
   }
 
   //for internal use only
@@ -216,7 +216,7 @@ abstract class cgsms_sender_base
       $res = $this->_send_fopen($cmd);
     else
       $res = $this->_send_curl($cmd);
-    debug_to_log('cgsms_sender_base - command = '.$cmd.' res = '.$res);
+    debug_to_log('smsg_sender_base - command = '.$cmd.' res = '.$res);
     return $res;
   }
 
@@ -260,18 +260,162 @@ abstract class cgsms_sender_base
   }
 
   /**
-  process_delivery_report:
+  get_setup_form:
+  Returns string, xhtml for echo into admin display
   */
-  public function process_delivery_report()
+  public function get_setup_form()
   {
-    return $this->_process_delivery_report();
+    $module = self::get_module();
+    $padm = $module->CheckPermission('AdministerSMSGateways');
+    $db = $module->GetDb();
+    $pref = cms_db_prefix();
+    $query = 'SELECT * FROM '.$pref.'module_smsg_gates WHERE alias=?';
+    if( !$padm )
+        $query .= ' AND enabled=1';
+    $alias = $this->get_alias();
+    $gdata = $db->GetRow($query,array($alias));
+    if(!$gdata)
+        return '';
+
+    $smarty = cmsms()->GetSmarty();
+    $smarty->assign('gatetitle',$module->Lang('frame_title',$gdata['title']));
+    $parms = array();
+    $query = 'SELECT gate_id,title,value,apiname,apiconvert FROM '.$pref.'module_smsg_props WHERE gate_id=?';
+    if( !$padm )
+        $query .= ' AND active=1';
+    $query .= ' ORDER BY apiorder';
+    $gid = (int)$gdata['gate_id'];
+    $res = $db->GetAll($query,array($gid));
+    if( $res )
+      {
+        foreach( $res as &$row )
+          {
+            $ob = (object)$row;
+            //adjustments
+            if( (int)$ob->apiconvert >= SMSG::DATA_PW ) //parameter is password
+              {
+                if( $module->havemcrypt ) //en/de-cryption possible
+                  {
+                    $e = new Encryption(MCRYPT_BLOWFISH,MCRYPT_MODE_CBC,SMSG::ENC_ROUNDS);
+                    $ob->value = $e->decrypt($ob->value,$module->GetPreference('masterpass')); //TODO dialog for user input
+                  }
+                $ob->pass = TRUE;
+              }
+            unset($ob->apiconvert);
+            $parms[] = $ob;
+          }
+        unset($row);
+      }
+    if($parms)
+      {
+        $smarty->assign('space',$alias); //for gateway-data 'namespace'
+        $smarty->assign('gateid',$gid);
+        $smarty->assign('dcount',count($parms));
+      }
+    else
+      {
+        $ob = new stdClass();
+        $ob->title = $module->Lang('error_nodatafound');
+        $ob->apiname = FALSE; //prevent input-object creation
+        $parms[] = $ob;
+        $smarty->assign('dcount',0);
+      }
+    $smarty->assign('data',$parms);
+    if( $padm )
+      {
+        $smarty->assign('titletitle',$module->Lang('title'));
+        $smarty->assign('titlevalue',$module->Lang('value'));
+        $smarty->assign('titleapiname',$module->Lang('apiname'));
+        $smarty->assign('titlehelp',$module->Lang('helptitle'));
+        $smarty->assign('titleselect',$module->Lang('select'));
+        $smarty->assign('help',
+         $module->Lang('help_dnd').'<br />'.$module->Lang('help_sure'));
+          $id = $smarty->tpl_vars['actionid']->value;
+        $text = $module->Lang('add_parameter');
+        $smarty->assign('additem',$module->CreateImageLink($id,$alias.'~add',
+         '',$text,'icons/system/newobject.gif',array(),'systemicon','',FALSE));
+        $smarty->assign('btndelete',$module->CreateInputSubmit($id,$alias.'~delete',
+         $module->Lang('delete'),'title="'.$module->Lang('delete_tip').
+         '" onclick="if(row_selected(event,this)) {return confirm(\''.$module->Lang('sure_ask').'\');} else {return false;}"')); //TODO js
+      }
+    // anything else to set up for the template
+    $this->custom_setup($smarty,$padm); //e.g. each $ob->size
+
+    $tpl = ($padm) ? 'gatedata_admin.tpl' : 'gatedata.tpl';
+    return $module->ProcessTemplate($tpl);
   }
+
+  /**
+  handle_setup_form:
+  @params: array of paramters provided after admin form 'submit'
+  Parses relevant @params into stored data
+  */
+  public function handle_setup_form($params)
+  {
+    $alias = $this->get_alias();
+    $module = self::get_module();
+    $db = $module->GetDb();
+    $pref = cms_db_prefix();
+    //TODO upsert needed
+    $sql1 = 'UPDATE '.$pref.'module_smsg_props SET ';
+    $sql2 = '=? WHERE gate_id=? AND apiname=?';
+
+    $gid = (int)$params[$alias.'~gate_id'];
+    unset($params[$alias.'~gate_id']);
+    foreach( $params as $key=>&$val )
+      {
+        if( strpos($key,$alias) === 0 )
+          {
+            $parts = explode('~',$key);
+            if( $parts[2] && $parts[2] != 'check' )
+              {
+$adbg = $db->Prepare($sql1.$parts[2].$sql2);
+$this->DoNothing();
+  
+               if( $parts[2] == 'apiname' )
+                 {
+                 //TODO injection check for $parts[1]
+                 }
+               elseif(0 && $val) //TODO test for password
+                 {
+                 $e = ( $module->havemcrypt ) ?
+                   new Encryption(MCRYPT_BLOWFISH,MCRYPT_MODE_CBC,SMSG::ENC_ROUNDS) : FALSE;
+                 $pw = $module->GetPreference('masterpass');
+                 if( $e && $pw ) $val = $e->encrypt($val,$pw);
+                 }
+               $db->Execute($sql1.$parts[2].$sql2,array($val,$gid,$parts[1]));
+              }
+          }
+      }
+    unset($val);
+    $this->custom_save($params);
+  }
+
+  //For internal use only
+  //Record or update gateway-specific details in the module's database tables
+  //Returns key-value of the row added to the gates-table, for the gateway
+  abstract public function upsert_tables();
+
+  //For internal use only
+  //Setup gateway-specific details for defaultadmin action
+  //$padm = boolean, TRUE if current user has AdministerSMSGateways permission
+  abstract public function custom_setup(&$smarty,$padm);
+
+  //For internal use only
+  //Process gateway-specific details after 'submit' in defaultadmin action
+  abstract public function custom_save(&$params);
 
   /**
   get_name:
   Returns string which is the (un-translated) gateway identifier
   */
   abstract public function get_name();
+
+  /**
+  get_alias:
+  Returns string which is the (un-translated) gateway alias, used for classname etc
+  */
+  abstract public function get_alias();
 
   /**
   get_description:
@@ -315,25 +459,27 @@ abstract class cgsms_sender_base
   */
   abstract public function support_mms();
 
+  //Perform pre-message-send initialisation or checks, if any
+  //Returns nothing
   abstract protected function setup();
 
+  //Construct the actual string to be sent to the gateway
+  //Returns that string, or else empty string or literal FALSE upon failure
+  //May return dummy e.g. 'good' or ' ' if such string isn't needed
   abstract protected function prep_command();
 
-  abstract protected function parse_result($str);
-
-  abstract protected function _process_delivery_report();
-
-  //for internal use only
-  //setup and display gateway-specific details for defaultadmin action
-  abstract public function get_setup_form();
-
-  //for internal use only
-  //process gateway-specific details after 'submit' in defaultadmin action
-  abstract public function handle_setup_form($params);
+  //Interpret $res (string or object, usually) returned from last message-send process
+  abstract protected function parse_result($res);
 
   /**
-  get_raw_status:
+  process_delivery_report:
+  Interpret message-delivery report and return resultant string, suitable for
+  public display or logging
   */
+  abstract public function process_delivery_report();
+
+  //For internal use only
+  //Get string returned by gateway in response to message-send process
   abstract public function get_raw_status();
 } // end of class
 #
