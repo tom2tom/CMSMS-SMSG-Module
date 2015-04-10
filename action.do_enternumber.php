b955bac47a9fd7a24451b3bed941254d8268196c
tom2tom <tpgww@onepost.net>
2015-04-11 08:33:27 +1000

    cleanups

--- action.do_enternumber.php
+++ action.do_enternumber.php
@@ -6,62 +6,52 @@
 # More info at http://dev.cmsmadesimple.org/projects/smsg
 #----------------------------------------------------------------------
 
-if( !isset($params['smskey']) ) return;  // no key
-
-$inline = 0;
-$thetemplate = $this->GetPreference(SMSG::PREF_DFLTENTERNUMBER_TPL);
-$error = '';
-$message = '';
-
-// verify that the text is still in the database...
-// and touch it just incase it would get expired.
+if( !isset($params['smskey']) )
+	return;  // no key
+// verify that the text is still cached
 $key = trim($params['smskey']);
 $datastore = new cge_datastore();
 $smstext = $datastore->get($this->GetName(),$key);
-if( !$smstext ) return;  // nothing to do
+if( !$smstext )
+	return;  // nothing to do
+// and touch the text to prevent expiry
 $datastore->erase($this->GetName(),$key);
 $datastore->store($smstext,$this->GetName(),$key);
 
-if( isset($params['enternumbertemplate']) )
-  {
-	$thetemplate = trim($params['enternumbertemplate']);
-  }
+$message = '';
+$error = '';
 
 if( isset($params['smsg_submit']) )
-  {
+{
 	// handle form submission
-	$mobile = '';
 	if( isset($params['smsg_mobile']) )
+	{
 		$mobile = trim($params['smsg_mobile']);
+		if( smsg_utils::is_valid_phone($mobile) )
+		{
+			// now we're ready to send
+			$gateway = smsg_utils::get_gateway($this);
+			if( $gateway )
+			{
+				$gateway->set_msg($smstext);
+				$gateway->set_num($mobile);
+				$gateway->send();
 
-	// data validation
-	if( !smsg_utils::is_valid_phone($mobile) )
-		$error = $this->Lang('error_invalid_number');
-
-	if( !$error )
-	  {
-		// now we're ready to send
-		$gateway = smsg_utils::get_gateway($this);
-		if( $gateway )
-		  {
-			$gateway->set_msg($smstext);
-			$gateway->set_num($mobile);
-			$gateway->send();
-
-			$stat = $gateway->get_status();
-			$msg = $gateway->get_statusmsg();
-			if( $stat == sms_gateway_base::STAT_OK )
-				$message = $this->Lang('sms_message_sent');
+				if( $gateway->get_status() == sms_gateway_base::STAT_OK )
+					$message = $this->Lang('sms_message_sent');
+				else
+					$error = $gateway->get_statusmsg();
+			}
 			else
-				$error = $msg;
-		  }
+			{
+				$error = $this->Lang('error_nogatewayfound');
+				$this->Audit('',$this->Lang('error_nogatewayfound'),'enternumber');
+			}
+		}
 		else
-		  {
-			$error = $this->Lang('error_nogatewayfound');
-			$this->Audit(0,$this->Lang('error_nogatewayfound'),'enternumber');
-		  }
-	  }
-  }
+			$error = $this->Lang('error_invalid_number');
+	}
+}
 
 // now display the form
 $smarty->assign('message',$message);
@@ -70,6 +60,10 @@ $smarty->assign('formstart',$this->CGCreateFormStart($id,'do_enternumber',
 	$returnid,$params));
 $smarty->assign('formend',$this->CreateFormEnd());
 
+if( empty($params['enternumbertemplate']) )
+	$thetemplate = $this->GetPreference(SMSG::PREF_ENTERNUMBER_TPLDFLT);
+else
+	$thetemplate = trim($params['enternumbertemplate']);
 echo $this->ProcessTemplateFromDatabase('enternumber_'.$thetemplate);
 
 ?>
