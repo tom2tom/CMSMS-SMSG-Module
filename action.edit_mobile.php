2a5afa7688fd824bb335c4560ee18ec19f550e45
tom2tom <tpgww@onepost.net>
2015-04-11 14:58:01 +1000

    cleanups

--- action.edit_mobile.php
+++ action.edit_mobile.php
@@ -13,89 +13,87 @@ $pref = cms_db_prefix();
 $this->SetCurrentTab('mobiles');
 
 if( isset($params['mid']) )
-  {
+{
     $mid = (int)$params['mid'];
-  }
+}
 
 if( $mid != '' )
-  {
-	$query = 'SELECT * FROM '.$pref.'module_smsg WHERE id=?';
+{
+	$query = 'SELECT * FROM '.$pref.'module_smsg_nums WHERE id=?';
 	$tmp = $db->GetRow($query,array($mid));
 	if( !$tmp )
-	  {
+	{
 		$this->SetError($this->Lang('error_notfound'));
 		$this->RedirectToTab($id);
-	  }
+	}
 	$name = $tmp['name'];
 	$mobile = $tmp['mobile'];
-  }
+}
 
 if( isset($params['cancel']) )
-  {
+{
 	$this->RedirectToTab($id);
-  }
+}
 else if( isset($params['submit']) )
-  {
+{
 	$name = trim($params['name']);
 	$mobile = trim($params['mobile']);
 	$error = '';
 
-	// do basic data checks
-	if( $name == '' || !is_numeric($mobile) )
-	  {
+	// basic data checks
+	if( !$name || !is_numeric($mobile) )
+	{
 		$error = $this->Lang('error_invalid_info');
-	  }
-
-	if( empty($error) )
-	  {
-		// check for duplicate name
-		$query = 'SELECT id FROM '.$pref.'module_smsg WHERE name=?';
-		$parms = array();
-		if( $mid != '' )
-		  {
-			$query .= ' AND id != ?';
-			$parms[] = $mid;
-		  }
-		$tmp = $db->GetOne($query,$parms);
-		if( $tmp )
-		  {
-			$error = $this->Lang('error_name_exists');
-		  }
-	  }
+	}
 
 	if( empty($error) )
 	{
-	// good to go... do add or insert
-	$dbr = '';
-	if( $mid == '' )
-	  {
-		// insert
-		$query = 'INSERT INTO '.$pref.'module_smsg (name,mobile) VALUES(?,?)';
-		$dbr = $db->Execute($query,array($name,$mobile));
-	  }
-	else
-	  {
-		// update
-		$query = 'UPDATE '.$pref.'module_smsg SET name=?, mobile=? WHERE id=?';
-		$dbr = $db->Execute($query,array($name,$mobile,$mid));
-	  }
+		// check for duplicate name
+		$query = 'SELECT id FROM '.$pref.'module_smsg_nums WHERE name=?';
+		$parms = array();
+		if( $mid != '' )
+		{
+			$query .= ' AND id!=?';
+			$parms[] = $mid;
+		}
+		$tmp = $db->GetOne($query,$parms);
+		if( $tmp )
+		{
+			$error = $this->Lang('error_name_exists');
+		}
+	}
 
-	if( !$dbr )
-	  {
-		$error = $this->Lang('error_db_op_failed');
-	  }
+	if( empty($error) )
+	{
+		// good to go... do add or insert
+		$res = '';
+		if( $mid == '' )
+		{
+			// insert
+			$query = 'INSERT INTO '.$pref.'module_smsg_nums (name,mobile) VALUES(?,?)';
+			$res = $db->Execute($query,array($name,$mobile));
+		}
+		else
+		{
+			// update
+			$query = 'UPDATE '.$pref.'module_smsg_nums SET name=?,mobile=? WHERE id=?';
+			$res = $db->Execute($query,array($name,$mobile,$mid));
+		}
+
+		if( !$res )
+		{
+			$error = $this->Lang('error_db_op_failed');
+		}
 	}
 
 	if( !empty($error) )
-	  {
+	{
 		$this->SetError($error);
-	  }
+	}
 	$this->RedirectToTab($id);
-  }
+}
 
-//
 // build the form
-//
 $smarty->assign('formstart',$this->CGCreateFormStart($id,'edit_mobile',$returnid,$params));
 $smarty->assign('formend',$this->CreateFormEnd());
 $smarty->assign('name',$name);
