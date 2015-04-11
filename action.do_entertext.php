309bcfb4fbb71971ebfd51e258a4e86269427ebf
tom2tom <tpgww@onepost.net>
2015-04-11 14:41:37 +1000

    cleanups

--- action.do_entertext.php
+++ action.do_entertext.php
@@ -8,61 +8,44 @@
 
 if( !isset($params['smsnum']) ) return;  // no number id
 
-$smsnum = (int)$params['smsnum'];
-$inline = 0;
-$thetemplate = $this->GetPreference(SMSG::PREF_DFLTENTERTEXT_TPL);
-$error = '';
+$smstext = '';
 $message = '';
 
-if( isset($params['enternumbertemplate']) )
-  {
-	$thetemplate = trim($params['enternumbertemplate']);
-  }
-
-//
-// verify that the SMS ID entered is valid
-//
-$query = 'SELECT mobile FROM '.cms_db_prefix().'module_smsg WHERE id=?';
+// check that the supplied SMS ID is valid
+$smsnum = (int)$params['smsnum'];
+$query = 'SELECT mobile FROM '.cms_db_prefix().'module_smsg_nums WHERE id=?';
 $mobile = $db->GetOne($query,array($smsnum));
-$smstext = '';
+$error = ( $mobile ) ? '' : $this->Lang('error_notfound');
 
-if( isset($params['smsg_submit']) )
-  {
-	$smstext = '';
+if( !$error && isset($params['smsg_submit']) )
+{
 	if( isset($params['smsg_smstext']) )
-	  {
 		$smstext = trim($params['smsg_smstext']);
-	  }
 
-	if( !smsg_utils::text_is_valid($smstext))
-	  {
-		$error = $this->Lang('error_invalid_text');
-	  }
-
-	if( !$error )
-	  {
+	if( smsg_utils::text_is_valid($smstext) )
+	{
 		// now we're ready to send
 		$gateway = smsg_utils::get_gateway($this);
 		if( $gateway )
-		  {
+		{
 			$gateway->set_msg($smstext);
 			$gateway->set_num($mobile);
 			$gateway->send();
 
-			$stat = $gateway->get_status();
-			$msg = $gateway->get_statusmsg();
-			if( $stat == sms_gateway_base::STAT_OK )
+			if( $gateway->get_status() == sms_gateway_base::STAT_OK )
 				$message = $this->Lang('sms_message_sent',$mobile);
 			else
-				$error = $msg;
-		  }
+				$error = $gateway->get_statusmsg();
+		}
 		else
-		  {
+		{
 			$error = $this->Lang('error_nogatewayfound');
-			$this->Audit(0,$this->Lang('error_nogatewayfound'),'entertext');
-		  }
-	  }
-  }
+			$this->Audit('',$this->Lang('error_nogatewayfound'),'entertext');
+		}
+	}
+	else
+		$error = $this->Lang('error_invalid_text');
+}
 
 // now display the form
 $smarty->assign('message',$message);
@@ -72,23 +55,10 @@ $smarty->assign('smstext',$smstext);
 $smarty->assign('formstart',$this->CGCreateFormStart($id,'do_entertext',
 	$returnid,$params));
 $smarty->assign('formend',$this->CreateFormEnd());
-$js = <<<EOS
-<script type="text/javascript">
-//<![CDATA[{literal}
-var st = null, cl = null;
-function smsg_entertext_onchange() {
- if(st == null) st = document.getElementById('smsg_entertext_smstext');
- var charsleft = 160 - st.value.length - 1;
- if(charsleft < 0) return false;
- if(cl == null) cl = document.getElementById('smsg_entertext_charsleft');
- cl.innerHTML = charsleft;
- return true;
-}
-//]]>{/literal}
-</script>
-
-EOS;
-$smarty->assign('js',$js);
+if( !isset($params['enternumbertemplate']) )
+	$thetemplate = $this->GetPreference(SMSG::PREF_ENTERTEXT_TPLDFLT);
+else
+	$thetemplate = trim($params['enternumbertemplate']);
 echo $this->ProcessTemplateFromDatabase('entertext_'.$thetemplate);
 
 ?>
