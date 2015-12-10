5a80e41857244fa95830093d797423b66512f181
tom2tom <tpgww@onepost.net>
2015-12-10 21:53:42 +1100

    omit irrelevant string

--- lang/en_US.php
+++ lang/en_US.php
@@ -63,7 +63,6 @@ $lang['error_invalid_text'] = 'The text you entered contains invalid characters'
 $lang['error_name_exists'] = 'An item by that name already exists';
 $lang['error_nodatafound'] = 'No data are available';
 $lang['error_nogatewaysfound'] = 'Could not find any SMS gateway';
-$lang['error_noparentclass'] = 'Could not find the CGExtensions module';
 $lang['error_notfound'] = 'Could not find the requested item';
 $lang['event_desc_delivery'] = 'Sent when a message-delivery-report is received from the current gateway (if such reports are enabled)';
 $lang['event_help_delivery'] = <<<EOS
