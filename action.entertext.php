e0ac547c862416657edb2863577826319a1d97e3
tom2tom <tpgww@onepost.net>
2015-12-11 05:50:56 +1100

    cleanups

--- action.entertext.php
+++ action.entertext.php
@@ -4,27 +4,32 @@
 # Copyright (C) 2015 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file SMSG.module.php
 # More info at http://dev.cmsmadesimple.org/projects/smsg
+# action: entertext - create a link that when clicked will display a form
+#  for the user to enter a message
 #----------------------------------------------------------------------
-
-$smsnum = '';
-$smstext = '';
-$urlonly = 0;
-$linktext = $this->Lang('send_me_message');
-$inline = 0;
 
 if(isset($params['smsnum']))
 {
 	$smsnum = (int)$params['smsnum'];
+	if($smsnum == '')
+		return; //don't know who to send to
 }
+else
+	return;
+
 if(isset($params['urlonly']))
 {
 	$urlonly = (int)$params['urlonly'];
 	unset($params['urlonly']);
 }
+else
+	$urlonly = 0;
+
 if(isset($params['inline']))
-{
 	$inline = (int)$params['inline'];
-}
+else
+	$inline = 0;
+
 if(isset($params['destpage']))
 {
 	$page = $this->resolve_alias_or_id($params['destpage']);
@@ -35,19 +40,16 @@ if(isset($params['destpage']))
 	}
 	unset($params['destpage']);
 }
+
 if(isset($params['linktext']))
 {
 	$linktext = trim($params['linktext']);
 	unset($params['linktext']);
 }
-if($smsnum == '')
-{
-	// don't know who to send to
-	return;
-}
+else
+	$linktext = $this->Lang('send_me_message');
 
-$txt = $this->CreateLink($id,'do_entertext',$returnid,$linktext,
-	$params,'',$urlonly,$inline);
-echo $txt;
+echo $this->CreateLink($id,'do_entertext',$returnid,$linktext,$params,'',
+	$urlonly,$inline);
 
 ?>
