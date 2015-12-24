840f5dca2e34e22577fe95eebb4dfdf1d683bf16
tom2tom <tpgww@onepost.net>
2015-12-25 10:56:16 +1100

    replace CGExtensions code support cancellation

--- action.savegates.php
+++ action.savegates.php
@@ -5,32 +5,36 @@
 # Refer to licence and other details at the top of file SMSG.module.php
 # More info at http://dev.cmsmadesimple.org/projects/smsg
 #----------------------------------------------------------------------
+if(!$this->CheckPermission('AdministerSMSGateways')) exit;
 
-$this->SetCurrentTab('gates');
+if(isset($params['cancel']))
+	$this->Redirect($id,'defaultadmin','',array('activetab'=>'gates'));
 
 $gateway = $params['sms_gateway']; //e.g. 'smsbroadcast' or -1
-if( !(isset($params['submit']) || isset($params[$gateway.'~delete'])) )
-	$this->RedirectToTab($id);
+if(!(isset($params['submit']) || isset($params[$gateway.'~delete'])))
+	$this->Redirect($id,'defaultadmin','',array('activetab'=>'gates'));
 
 $objs = smsg_utils::get_gateways_full($this);
-if( $objs )
+if($objs)
 {
-	if( isset($params['submit']) )
+	if(isset($params['submit']))
 	{
 		$pref = cms_db_prefix();
 		$sql = 'UPDATE '.$pref.'module_smsg_gates SET active=0 WHERE active=1';
 		$db->Execute($sql);
-		if( $gateway != '-1' )
+		if($gateway != '-1')
 		{
 			$sql = 'UPDATE '.$pref.'module_smsg_gates SET enabled=1,active=1 WHERE alias=?';
 			$db->Execute($sql,array($gateway));
 		}
 	}
 
-	foreach( $objs as $classname => $rec )
+	foreach($objs as $classname=>$rec)
+	{
 		$rec['obj']->handle_setup_form($params);
+	}
 }
 
-$this->RedirectToTab($id);
+$this->Redirect($id,'defaultadmin','',array('activetab'=>'gates'));
 
 ?>
