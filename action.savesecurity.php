6b8468d25c7d96ba8456daf06d206a87ff3ce946
tom2tom <tpgww@onepost.net>
2017-03-05 14:20:33 +1100

    apply namespaces

--- action.savesecurity.php
+++ action.savesecurity.php
@@ -1,72 +1,64 @@
 <?php
 #----------------------------------------------------------------------
 # This file is part of CMS Made Simple module: SMSG
-# Copyright (C) 2015-2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2015-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file SMSG.module.php
 # More info at http://dev.cmsmadesimple.org/projects/smsg
 #----------------------------------------------------------------------
-if(!$this->CheckPermission('AdministerSMSGateways')) exit;
+if (!$this->CheckPermission('AdministerSMSGateways')) exit;
 
-if(isset($params['cancel']))
+if (isset($params['cancel'])) {
 	$this->Redirect($id,'defaultadmin','',['activetab'=>'security']);
-
+}
 $this->SetPreference('hourlimit',(int)$params['hourlimit']);
 $this->SetPreference('daylimit',(int)$params['daylimit']);
 $this->SetPreference('logsends',!empty($params['logsends']));
 $this->SetPreference('logdays',(int)$params['logdays']);
 $this->SetPreference('logdeliveries',!empty($params['logdeliveries']));
-if(isset($params['masterpass']))
-{
-	$oldpw = $this->GetPreference('masterpass');
-	if($oldpw)
-		$oldpw = smsg_utils::unfusc($oldpw);
+if (isset($params['masterpass'])) {
+	$cfuncs = new SMSG\Crypter($this);
+	$oldpw = $cfuncs->decrypt_preference('masterpass');
 	$newpw = trim($params['masterpass']);
-	if($oldpw != $newpw)
-	{
+	if ($oldpw != $newpw) {
 		//update current passwords
 		$pref = cms_db_prefix();
 		$sql = 'SELECT gate_id,title,value,encvalue FROM '.$pref.'module_smsg_props WHERE encrypt>0';
 		$rows = $db->GetArray($sql);
-		if($rows)
-		{
-			$e = ($this->havemcrypt) ?
-				new Encryption('BF-CBC','default',SMSG::STRETCHES) : FALSE; //no namespace
-			if($e && $newpw)
-			{
+		if ($rows) {
+			if ($newpw) {
 				$tofield = 'encvalue';
 				$notfield = 'value';
 				$encval = 1;
-			}
-			else
-			{
+			} else {
 				$tofield = 'value';
 				$notfield = 'encvalue';
 				$encval = 0;
 			}
 			$sql = 'UPDATE '.$pref.'module_smsg_props SET '.$tofield.'=?,'.$notfield.'=NULL,encrypt=? WHERE gate_id=? AND title=?';
-			foreach($rows as &$onerow)
-			{
-				if($oldpw)
-					$raw = ($e && $onerow['encvalue']) ? $e->decrypt($onerow['encvalue'],$oldpw) : $onerow['encvalue'];
-				else
+			foreach ($rows as &$onerow) {
+				if ($oldpw) {
+					$raw = ($onerow['encvalue']) ?
+						$cfuncs->decrypt_value($onerow['encvalue'],$oldpw) :
+						NULL;
+				} else {
 					$raw = $onerow['value'];
-				if($newpw)
-					$revised = ($raw && $e) ? $e->encrypt($raw,$newpw) : $raw;
-				else
+				}
+				if ($newpw) {
+					$revised = ($raw) ?
+						$cfuncs->encrypt_value($raw,$newpw) :
+						NULL;
+				} else {
 					$revised = $raw;
-				if(!$revised)
+				}
+				if (!$revised) {
 					$revised = NULL;
+				}
 				$db->Execute($sql,[$revised,$encval,$onerow['gate_id'],$onerow['title']]);
 			}
 			unset($onerow);
 		}
-		if($newpw)
-			$newpw = smsg_utils::fusc($newpw);
-
-		$this->SetPreference('masterpass',$newpw);
+		$cfuncs->encrypt_preference('masterpass',$newpw);
 	}
 }
 
 $this->Redirect($id,'defaultadmin','',['activetab'=>'security']);
-
-?>
--- action.smstest.php
+++ action.smstest.php
@@ -1,42 +1,38 @@
 <?php
 #----------------------------------------------------------------------
 # This file is part of CMS Made Simple module: SMSG
-# Copyright (C) 2015-2016 Tom Phane <tpgww@onepost.net>
+# Copyright (C) 2015-2017 Tom Phane <tpgww@onepost.net>
 # Refer to licence and other details at the top of file SMSG.module.php
 # More info at http://dev.cmsmadesimple.org/projects/smsg
 #----------------------------------------------------------------------
-if(!($this->CheckPermission('AdministerSMSGateways')
+if (!($this->CheckPermission('AdministerSMSGateways')
   || $this->CheckPermission('ModifySMSGateways'))) exit;
 
-if(isset($params['submit']))
-{
+if (isset($params['submit'])) {
 	$number = '';
-	if(isset($params['mobile']))
+	if (isset($params['mobile'])) {
 		$number = trim($params['mobile']);
-
-	if(smsg_utils::is_valid_phone($number))
-	{
+	}
+	if (SMSG\Utils::is_valid_phone($number)) {
 		// ready to test (default gateway)
-		$gateway = smsg_utils::get_gateway(FALSE,$this);
-		if($gateway)
-		{
+		$gateway = SMSG\Utils::get_gateway(FALSE,$this);
+		if ($gateway) {
 			$gateway->set_num($number);
 			$gateway->set_msg($this->Lang('test_message',SMSG::MODNAME.' @ '.strftime('%X %Z')));
 			$gateway->send();
 			$status = $gateway->get_status();
 			$msg = $gateway->get_statusmsg();
-			if($status == base_sms_gateway::STAT_OK)
+			if ($status == SMSG\base_sms_gateway::STAT_OK) {
 				$this->SetMessage($msg);
-			else
+			} else {
 				$this->SetError($msg);
-		}
-		else
+			}
+		} else {
 			$this->SetError($this->Lang('error_nogatewayfound'));
-	}
-	else
+		}
+	} else {
 		$this->SetError($this->Lang('error_invalid_number'));
+	}
 }
 
 $this->Redirect($id,'defaultadmin','',['activetab'=>'test']);
-
-?>
